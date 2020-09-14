3cc7a904d7b888e7213f48b22a9d5f6d12171fbc
Eric Piacentini <ericpiacentini1@gmail.com>
2020-09-14 12:18:10 -0700

    Initial Commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const botName = 'chatBot';
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+  socket.emit('message', formatMessage(botName, 'Welcome to Chat'));
+
+  socket.broadcast.emit(
+    'message',
+    formatMessage(botName, 'A user has joined the chat')
+  );
+
+  socket.on('disconnect', () => {
+    io.emit('message', formatMessage(botName, 'A user has left the chat'));
+  });
+
+  socket.on('chatMessage', (msg) => {
+    io.emit('message', formatMessage('USER', msg));
+  });
+});
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server Running on ${PORT}`));
